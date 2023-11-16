a3ef2ccb7cf843699f8485710f2917ab7b394bea
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-11-16 14:40:19 -0500

    Create frontend folder

---
+++ frontend/src/lib/web3.ts
@@ -0,0 +1,23 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { optimism, mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+export const chains = [optimism, mainnet]
+
+export const { publicClient, webSocketPublicClient } = configureChains(chains, [
+  publicProvider(),
+])
+
+const { connectors } = getDefaultWallets({
+  appName: 'Team Nick',
+  projectId: 'd6c989fb5e87a19a4c3c14412d5a7672',
+  chains,
+})
+
+export const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
