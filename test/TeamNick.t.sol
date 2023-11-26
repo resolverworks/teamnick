b0f93e0e286456d6b7d1f146373cfd56d9d7573d
lcfr <me@lcfr.io>
2023-11-26 13:53:29 -0500

    test

--- test/TeamNick.t.sol
+++ test/TeamNick.t.sol
@@ -17,26 +17,31 @@ contract TeamNickTest is Test {
     TeamNick public teamnick;
 
     function setUp() public {
+	// initial owner
         address initialOwner = address(123);
+	// baseuri
 	string memory baseUri = "http//www.google.com/";
         teamnick = new TeamNick(initialOwner, baseUri);
     }
 
     function test_RegisterAndTransfer() public {
+	// impersonate 123 address
 	vm.startPrank(address(123));
+	// give it 100 ether
 	vm.deal(address(123), 100 ether);
 
-        console2.log("Deployed to: ", address(teamnick));
+	// test registering a name
 	teamnick.register("lcfr", address(123), address(123), "http://msn.com");
+
 	// node for lcfr is 71556845390930511116542978182046292420618941324241853217323411142934832950810
 
-
-	teamnick.setAddr(71556845390930511116542978182046292420618941324241853217323411142934832950810, address(666));
-
+	// setapprovalforall for seaport 1.5
 	teamnick.setApprovalForAll(0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC, true);
-
+	// stop impersonating 123 address
 	vm.stopPrank();
+	// start impersonating seaport 1.5
 	vm.startPrank(0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC);
+	// try to transfer as approved seaport contract
 	teamnick.safeTransferFrom(address(123), address(321), 71556845390930511116542978182046292420618941324241853217323411142934832950810);
     }
 }
