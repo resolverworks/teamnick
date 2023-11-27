d06fbb99f73a8d1bccbc5115c184d4accd7fa4e8
aslobodnik <aslobodnik@gmail.com>
2023-11-27 11:25:42 -0500

    add og:image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,6 +8,14 @@ import { ClientProviders } from '@/lib/providers'
 export const metadata: Metadata = {
   title: 'Team Nick',
   description: 'A name a day, keeps the bad vibes away',
+  openGraph: {
+    images: [
+      {
+        url: '/imagepreview.png',
+        alt: 'Team Nick Image Preview',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
