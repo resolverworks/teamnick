4e560126e48894dc6300e88ab32f6e6d674ada43
aslobodnik <aslobodnik@gmail.com>
2023-12-13 19:23:11 -0500

    update ponder to pull first 1000

--- frontend/src/hooks/usePonder.ts
+++ frontend/src/hooks/usePonder.ts
@@ -7,18 +7,22 @@ export function usePonder() {
   const [cacheKey, setCacheKey] = useState('')
 
   const graphQlQuery = `
-    query {
-      profiles (orderBy: "registeredAt", orderDirection: "desc") {
-        id
-        name
-        label
-        owner
-        address
-        avatar
-        registeredAt
-      }
+  query ProfilesQuery($first: Int!) {
+    profiles (orderBy: "registeredAt", orderDirection: "desc", first: $first) {
+      id
+      name
+      label
+      owner
+      address
+      avatar
+      registeredAt
     }
-  `
+  }
+`
+
+  const variables = {
+    first: 1000, // set the value of 'first' here
+  }
 
   const { data, error } = useFetch<PonderResponse<{ profiles: Profile[] }>>(
     ponderUrl + '?key=' + cacheKey,
@@ -27,7 +31,10 @@ export function usePonder() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ query: graphQlQuery }),
+      body: JSON.stringify({
+        query: graphQlQuery,
+        variables: variables, // include the variables in the request
+      }),
     }
   )
 
